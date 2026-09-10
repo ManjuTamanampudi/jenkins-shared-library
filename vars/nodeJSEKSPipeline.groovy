0695eef7b2487728f73a27d0ed92ba2ae3af93c4
Manju Tamanampudi <manjukarri3@gmail.com>
2026-09-10 14:57:25 +0530

    jenkins-sharedlibrary

--- vars/nodeJSEKSPipeline.groovy
+++ vars/nodeJSEKSPipeline.groovy
@@ -2,7 +2,7 @@ def call(Map configMap){
     pipeline {
         agent {
             node {
-                label 'roboshop' 
+                label 'ROBOSHOP' 
             } 
         }
         environment {
